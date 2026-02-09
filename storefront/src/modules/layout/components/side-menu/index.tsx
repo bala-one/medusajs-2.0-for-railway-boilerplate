a09194964f6ca9d67fc30a1823e428892d20105c
Balaji <bkandasamy@ceiamerica.com>
2026-02-09 08:21:05 -0500

    Title updates

--- storefront/src/modules/layout/components/side-menu/index.tsx
+++ storefront/src/modules/layout/components/side-menu/index.tsx
@@ -91,7 +91,7 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
                         />
                       </div>
                       <Text className="flex justify-between txt-compact-small">
-                        © {new Date().getFullYear()} Medusa Store. All rights
+                        © {new Date().getFullYear()} UPMC Optical Store. All rights
                         reserved.
                       </Text>
                     </div>
