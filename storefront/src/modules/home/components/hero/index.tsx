d7ac1b2a6d5b7ddb4d0406df539566a0aeba95f2
Balaji <bkandasamy@ceiamerica.com>
2026-02-09 21:11:24 -0500

    Image try again

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -2,16 +2,19 @@ import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
 import Image from 'next/image'
 
-const heroImg = '/images/hero.jpg'
+const heroImage = '/images/hero.jpg'
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
       <Image
-        src={heroImg}
+        src={heroImage}
         alt="Description of the image"
         priority // Add 'priority' for images above the fold
-      />
+        width={1000}
+        height={600}
+        />
+       <img src="/images/hero.jpg" alt="Hero" />
       </div>
     </div>
   )
