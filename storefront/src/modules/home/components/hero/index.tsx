e69e4d28c266397bcd82ce7a7fec8fe15e7b94ed
Balaji <bkandasamy@ceiamerica.com>
2026-02-09 21:33:51 -0500

    New image update

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-       <img src="/images/hero.jpg" alt="UPMC Optical Store" />
+        <img src="/images/herobanner.jpg" alt="UPMC Optical Store" />
       </div>
     </div>
   )
